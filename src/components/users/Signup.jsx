ede7ce49dd85161945dfdec048f1dbf04f9f1848
nggoong <nggoong@naver.com>
2022-07-15 23:19:46 +0900

    회원가입 기능 구현 완료

--- src/components/users/Signup.jsx
+++ src/components/users/Signup.jsx
@@ -1,11 +1,36 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
 import {LoginWrapper, LoginBox} from './Login';
+import { auth, db } from '../../shared/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Signup = () => {
+    const [inputs, setInputs] = useState({
+        email:'',
+        nickname:'',
+        password:'',
+        password_confirm:''
+    })
     const firstInputRef = useRef(null);
     const navigate = useNavigate();
+
+    const inputChangeHandler = (e) => {
+        const {name, value} = e.target;
+        setInputs({...inputs, [name]:value});
+    }
+
+    const signupBtnClickHandler = async () => {
+        const {email, nickname, password, ...rest} = inputs;
+        const user = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(user);
+        const dbCollection = collection(db, 'users');
+        const userDoc = await addDoc(dbCollection, {user_id:user.user.email, nickname:nickname})
+        console.log(userDoc.id);
+        alert('회원 가입이 완료되었습니다.')
+        navigate('/member/login');
+    }
 
     useEffect(()=> {
         firstInputRef.current.focus();
@@ -14,13 +39,13 @@ const Signup = () => {
         <SignupWrapper>
             <SignupBox>
             <div className='logo-area'>
-                <img src='/Joologo.PNG' onClick={()=>navigate('/')}></img>
+                <img alt='logo-image' src='/Joologo.PNG' onClick={()=>navigate('/')}></img>
             </div>
-            <input type='email' placeholder='email' ref={firstInputRef}/>
-            <input type='text' placeholder='nickname'/>
-            <input type='password' placeholder='password'/>
-            <input type='password' placeholder='password confirm'/>
-            <button>회원가입</button>
+            <input type='email' placeholder='email' ref={firstInputRef} name='email' value={inputs.email} onChange={inputChangeHandler}/>
+            <input type='text' placeholder='nickname' name='nickname' value={inputs.nickname} onChange={inputChangeHandler}/>
+            <input type='password' placeholder='password' name='password' value={inputs.password} onChange={inputChangeHandler}/>
+            <input type='password' placeholder='password confirm' name='password_confirm' value={inputs.password_confirm} onChange={inputChangeHandler}/>
+            <button onClick={signupBtnClickHandler}>회원가입</button>
             </SignupBox>
         </SignupWrapper>
     )

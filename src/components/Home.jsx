4545b656fae652525fc7c43c415099ec354c45b3
nggoong <nggoong@naver.com>
2022-07-21 01:25:53 +0900

    레이아웃 결정을 위해 layout props를 넘김

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import PostingCard from './posting/PostingCard';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,7 +7,6 @@ import { fsActions } from '../redux/module/fsReducer';
 const Home = () => {
     const postingList = useSelector(state=> state.posting.postings);
     const dispatch = useDispatch();
-    console.log('home render')
     useEffect(()=> {
         // dispatch(postingActions.setDefaultPostings());
         console.log('home effect')
@@ -21,7 +20,7 @@ const Home = () => {
     return(
         <PostingViewerWrapper>
             {postingList.map((item, index) => <PostingCard key={item.docID}
-            nickname={item.user_nickname} text={item.text} url={item.image_url} idx={index} length={postingList.length}/>)}
+            nickname={item.user_nickname} text={item.text} url={item.image_url} idx={index} length={postingList.length} layout={item.layout}/>)}
         </PostingViewerWrapper>
     )
 }
